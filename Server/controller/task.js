98f8a17ad35e7744566950c114b4b7970ae3fcb8
nuralam <nurcse253@gmail.com>
2024-04-05 23:20:41 +0600

    password encrypted & json web token

--- Server/controller/task.js
+++ Server/controller/task.js
@@ -2,8 +2,6 @@ import express from "express";
 import Task from "../model/Task.js";
 const taskRouter = express.Router();
 
-
-
 taskRouter.post("/", async (req, res) => {
   try {
     const { title, body, status } = req.body;
@@ -24,15 +22,26 @@ taskRouter.post("/", async (req, res) => {
 taskRouter.get("/", async (req, res) => {
   const page = parseInt(req.query.page) || 1;
   const limit = parseInt(req.query.limit) || 10;
+  const sortby = req.query.sort || "Descending";
+  const status = req.query.status || "All";
+  let sort = -1;
 
   try {
     const totalPosts = await Task.countDocuments();
     const totalPages = Math.ceil(totalPosts / limit);
 
-    const tasks = await Task.find()
+    if (sortby === "Ascending") sort = 1;
+
+    let query = {};
+    if (status !== "All") {
+      query.status = status;
+    }
+
+    const tasks = await Task.find(query)
       .populate("user", "-password")
       .limit(limit)
       .skip((page - 1) * limit)
+      .sort({ createdAt: sort })
       .exec();
 
     res.json({
@@ -58,15 +67,13 @@ taskRouter.delete("/:id", async (req, res) => {
   }
 });
 
-
 taskRouter.put("/:id", async (req, res) => {
   try {
-    const {id} = req.params
-    const {title,body,status} = req.body
-     await Task.findByIdAndUpdate({_id:id},{title,body,status})
+    const { id } = req.params;
+    const { title, body, status } = req.body;
+    await Task.findByIdAndUpdate({ _id: id }, { title, body, status });
 
-     res.json({message:"Successfully updated"})
-
+    res.json({ message: "Successfully updated" });
   } catch (err) {
     console.log(err);
     res.status(500).json({ error: err.message });
--- Server/controller/user.js
+++ Server/controller/user.js
@@ -1,19 +1,50 @@
 import express from "express";
+import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken'
+
 import User from "../model/User.js";
 const userRouter = express.Router();
 
-
 userRouter.post("/", async (req, res) => {
   try {
-    const { fullName,email,password } = req.body;
+    let { fullName, email, password } = req.body;
 
-    const user = new User({ fullName,email,password});
+    password = await bcrypt.hash(password || "", 10);
+    const user = new User({ fullName, email, password });
     await user.save();
     res.status(201).json({ message: "Successfully registered user" });
   } catch (err) {
-    res.status(400).json({ message: err.message });
+    res.status(500).json({ message: err.message });
   }
 });
 
 
-export{userRouter}
+userRouter.post("/login", async (req, res) => {
+  try {
+    const {email,password} = req.body
+    const existUser = await User.findOne({email})
+   
+    if(!existUser) return res.status(404).json({error:"Didn't found the user"})
+
+     const isMatch = await bcrypt.compare(password, existUser.password);
+    if (!isMatch) return res.status(401).json({error: "Wrong password"});
+
+    const token = jwt.sign(
+      {
+        userName: existUser.userName,
+        email: existUser.email,
+      },
+      "SKDFDLK09UU@@@SKDJDHF2233"
+    );
+
+    res.status(200).json({
+      message: "Login success",
+      token: token,
+      userName: existUser.fullName
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+export { userRouter };
